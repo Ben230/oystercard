e4fb9b370aa6db80c8178fc17139598fdbc57f95
Ben Stuart <benarthurstuart@hotmail.com>
2019-10-21 16:24:18 +0100

    add max balance constant, raises error if top up over the limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,18 @@
 class Oystercard
+  MAXIMUM_BALANCE = 90
   def initialize
     @balance = 0
   end
   attr_reader :balance
 
   def top_up(amount)
+    raise "max balance of #{MAXIMUM_BALANCE} reached" if max?(amount)
     @balance += amount
   end
-    
+
+  private
+
+  def max?(amount)
+    @balance + amount > MAXIMUM_BALANCE
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,5 +11,11 @@ describe Oystercard do
     it 'tops up the balance by the top up amount' do
       expect { subject.top_up 1 }.to change { subject.balance }.by 1
     end
+
+    it 'raises an error if the max balance has been reached' do
+      max_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(max_balance)
+      expect{ subject.top_up(1) }.to raise_error "max balance of #{max_balance} reached"
+    end
   end
 end
